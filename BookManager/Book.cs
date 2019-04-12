fd1f2d3271fc8b3f79e2abc311feb42560cdc281
The Very Cool School Pc <ddd.com>
2019-04-12 10:59:37 +0100

    constructor and comments

--- BookManager/Book.cs
+++ BookManager/Book.cs
@@ -6,11 +6,13 @@ namespace BookManager
 {
     class Book
     {
+        //private variables
         private string title;
         private string author;
         private int pages;
         private int wordCount;
 
+        //constructor
         public Book(string title, string author)
         {
             this.title = title;
